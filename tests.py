dcc72d396330cf178a59788179b7ddee64247fad
Tooblippe <tobie.nortje@gmail.com>
2014-04-04 12:19:34 +0200

    update requirements - added xlrd

--- tests.py
+++ tests.py
@@ -9,7 +9,12 @@ from waterfall import Waterfall
 
 def testWaterfall():
     df = pandas.ExcelFile('test.xls').parse("Sheet1")
-    assert Waterfall(df, fig_size=(11, 6), xticks_fontsize=9, outfile="temp.png") == 'OK'
+    values = df['Rbn']
+    xtick_names = df['Values']
+    assert Waterfall(values, xtick_names, fig_size=(11, 6), xticks_fontsize=9, outfile="temp.png") == 'OK'
+
+    
+
 
 
 
